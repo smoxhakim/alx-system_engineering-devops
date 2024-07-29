bafb84dc3c5142aaff6238bc6abe12b4ed031f93
smoxhakim <joulal1994@gmail.com>
2024-07-29 22:11:19 +0100

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""using this REST API, for a given employee ID"""
+import requests
+import sys
+
+employee_id = int(sys.argv[1])
+user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+user_response = requests.get(user_url)
+todos_response = requests.get(todos_url)
+
+if user_response.status_code != 200 or todos_response.status_code != 200:
+    print("Error fetching data from the API")
+    sys.exit(1)
+
+user = user_response.json()
+todos = todos_response.json()
+employee_name = user['name']
+employee_tasks = [task for task in todos if task['userId'] == employee_id]
+
+total_tasks = len(employee_tasks)
+completed_tasks = [task for task in employee_tasks if task['completed']]
+n_completed_tasks = len(completed_tasks)
+
+
+print(f"Employee {employee_name} is done with tasks\
+      ({n_completed_tasks}/{total_tasks}):")
+for task in completed_tasks:
+    print(f"\t {task['title']}")
