6a24e94e8735a0315a14c5ccd84d9f65f18fc0f4
smoxhakim <joulal1994@gmail.com>
2024-07-29 23:33:09 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,10 +12,6 @@ if __name__ == "__main__":
                                 .format(employee_id))
     todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
 
-    """if user_respons.status_code != 200 or todos_response.status_code != 200:
-        print("Error fetching data from the API")
-        sys.exit(1)
-    """
     user = user_respons.json()
     todos = todos_response.json()
     employee_name = user['name']
@@ -25,7 +21,7 @@ if __name__ == "__main__":
     completed_tasks = [task for task in employee_tasks if task['completed']]
     n_completed_tasks = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks\
-        ({n_completed_tasks}/{total_tasks}): ")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, n_completed_tasks, total_tasks))
     for task in completed_tasks:
-        print(f"\t {task['title']}")
+        print("\t {}".format(task.get("title")))
