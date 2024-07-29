ff9bc3aeb4f4a553cd90fbe1c911c6835bb732e3
smoxhakim <joulal1994@gmail.com>
2024-07-29 22:58:07 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,8 @@ import sys
 
 
 employee_id = int(sys.argv[1])
-user_response = requests.get("https://jsonplaceholder.typicode.com/users/{employee_id}")
+user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                             .format(employee_id))
 todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
 
 if user_response.status_code != 200 or todos_response.status_code != 200:
@@ -23,6 +24,6 @@ n_completed_tasks = len(completed_tasks)
 
 
 print(f"Employee {employee_name} is done with tasks\
-      ({n_completed_tasks}/{total_tasks}):")
+      ({n_completed_tasks}/{total_tasks}): ")
 for task in completed_tasks:
     print(f"\t {task['title']}")
