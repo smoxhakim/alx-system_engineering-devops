2b2129f96ec2f2680194cd279f8282f885130dd4
smoxhakim <joulal1994@gmail.com>
2024-07-29 23:03:22 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-import requests
 import sys
+import requests
+
 """using this REST API, for a given employee ID"""
 
+if __name__ == "__main__":
 
-employee_id = int(sys.argv[1])
-user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                             .format(employee_id))
-todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    employee_id = int(sys.argv[1])
+    user_respons = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                                .format(employee_id))
+    todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
 
-if user_response.status_code != 200 or todos_response.status_code != 200:
-    print("Error fetching data from the API")
-    sys.exit(1)
+    if user_respons.status_code != 200 or todos_response.status_code != 200:
+        print("Error fetching data from the API")
+        sys.exit(1)
 
-user = user_response.json()
-todos = todos_response.json()
-employee_name = user['name']
-employee_tasks = [task for task in todos if task['userId'] == employee_id]
+    user = user_respons.json()
+    todos = todos_response.json()
+    employee_name = user['name']
+    employee_tasks = [task for task in todos if task['userId'] == employee_id]
 
-total_tasks = len(employee_tasks)
-completed_tasks = [task for task in employee_tasks if task['completed']]
-n_completed_tasks = len(completed_tasks)
+    total_tasks = len(employee_tasks)
+    completed_tasks = [task for task in employee_tasks if task['completed']]
+    n_completed_tasks = len(completed_tasks)
 
-
-print(f"Employee {employee_name} is done with tasks\
-      ({n_completed_tasks}/{total_tasks}): ")
-for task in completed_tasks:
-    print(f"\t {task['title']}")
+    print(f"Employee {employee_name} is done with tasks\
+        ({n_completed_tasks}/{total_tasks}): ")
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
