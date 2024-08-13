87c3dbe0969bae1d4bfbf715756e23e569da0b52
smoxhakim <joulal1994@gmail.com>
2024-08-13 18:11:08 +0100

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Recursive function"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    if after:
+        url += f"?after={after}"
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Bio/34'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()['data']
+    hot_list.extend(post['data']['title'] for post in data['children'])
+    if data['after'] is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, data['after'])
