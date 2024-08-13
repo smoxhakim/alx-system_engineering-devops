d8c5a8ff7c8e5bc6743f1ae4fb5f9eb3769fd08d
smoxhakim <joulal1994@gmail.com>
2024-08-13 16:35:09 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""Returns the number of subscribers of a subreddit"""
+"""This script"""
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Edge109.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """Method get the number of users subscribed to a subreddit
+    """
+    try:
+        h = {'user-agent': 'Mozilla/5.0', 'allow_redirects': 'false'}
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        req = requests.get(url, headers=h)
+        return req.json().get('data').get('subscribers', 0)
+    except Exception as e:
         return 0
-    return response.json()['data']['subscribers']
 
 
 if __name__ == "__main__":
